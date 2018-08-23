96ae34a44ca27e257d3ef3c6f6bc18206945d927
Xhacker's New mac <pengchuan@loocall.com>
2018-08-23 15:17:35 +0800

    update sript

--- re_biuld_docker.sh
+++ re_biuld_docker.sh
@@ -1,4 +1,10 @@
 git pull
-sudo docker stop `sudo docker ps -qa`
-sudo docker rm `sudo docker ps -qa`
+CONTAINER_COUNT=`sudo docker ps -qa|wc -l`
+
+echo "${CONTAINER_COUNT}"
+if [ ${CONTAINER_COUNT} -ne 0 ]; then
+    sudo docker stop `sudo docker ps -qa`
+    sudo docker rm `sudo docker ps -qa`
+fi
+
 sudo docker -t layacloud-node_layanode .
